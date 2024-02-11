2c97bc5e54d3c75c616495e7c15f8424810b8a03
Ayushi Gupta <mcs.ayushigupta@gmail.com>
2024-02-11 12:43:02 +0530

    navbar styling

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <Box width="400px">
+    <>
+    <Box width="400px" sx={{width:{xl:'1488px'}}} m='auto'>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home/>} />
@@ -17,8 +18,9 @@ function App() {
       </Routes>
       <Footer />
     </Box>
+    </>
   );
 }
 
-
+export default App;
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      
+      Footer 
     </div>
   )
 }
 
-export default Footer
+export default Footer;
